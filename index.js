3ee0c25ddcbdb4d79f501d51cc520955171827e0
Asifazeem148 <161632854+Asifazeem148@users.noreply.github.com>
2024-03-11 21:49:10 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+//Q2  Personal Message: 
+var personName = ("hello john, ");
+var message = "would you like to learn some TypeScript today?”";
+console.log(personName + message);
---
+++ index.ts
@@ -0,0 +1,8 @@
+//Q2  Personal Message: 
+
+ 
+let personName: string = ("hello john, ");
+
+let message: string = "would you like to learn some TypeScript today?”"
+
+console.log (personName + message);
